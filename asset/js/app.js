dd6361fde96898bd1ed5176e5da1fd4c4bfdb750
estherlaraaaa <00049717@uca.edu.sv>
2018-10-19 11:18:26 -0600

    terminado

--- asset/js/app.js
+++ asset/js/app.js
@@ -25,11 +25,18 @@ const navSlide = () => {
 navSlide();
 /*********************************Colocar aca el desarrollo de su ejercicio***************************/
 
-var cont e = 1;
+var cont = 1;
 
 var bitacoras = [];
 
 var formulario = document.getElementById("bitacora");
+
+var fch = document.getElementById("fecha");
+var de = document.getElementById("descp");
+var ca = document.getElementById("cant");
+var fechaEstado = false;
+var descEstado = false;
+var caEstado = false;
 
 formulario.addEventListener("submit", (evt) => {
     evt.preventDefault();
@@ -39,9 +46,35 @@ formulario.addEventListener("submit", (evt) => {
         descripcion: formulario[2].value,
         cantidad: formulario[3].value
     }
-    bitacoras.push(bitacora);
-    cont++;
-    mostrar();
+    if (formulario[1].value === "" || formulario[1].value === null){
+        fch.style.borderColor = "red";
+        fechaEstado = false; 
+    }else {
+        fch.style.borderColor = "green";
+        fechaEstado = true; 
+    }
+    if (formulario[2].value === "" || formulario[2].value === null){
+        de.style.borderColor = "red"; 
+        descEstado = false;
+    }else {
+        de.style.borderColor = "green";
+        descEstado = true;
+    }
+    if(formulario[3].value === "" || formulario[3].value === null){
+        ca.style.borderColor = "red"; 
+        caEstado = false;
+    }else{
+        ca.style.borderColor = "green";
+        caEstado = true;
+    }
+    if(fechaEstado && descEstado && caEstado){
+        bitacoras.push(bitacora);
+        cont++;
+        mostrar();
+    }else{
+        alert("Tus campos estan incorrectos");
+    }
+    
 });
 
 const crearElemento = (bitacora, tbody) => {
@@ -80,5 +113,12 @@ const mostrar = () => {
     bitacoras.forEach(item => {
         crearElemento(item, document.querySelector(".tabla-btc tbody"));
     });
-    agregar();
+    agregar(); 
 }
+
+
+
+
+
+
+
