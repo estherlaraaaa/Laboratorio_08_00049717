f44ec0f1c1bc63d3d23361290aea9d6e7c4da070
estherlaraaaa <00049717@uca.edu.sv>
2018-10-19 10:15:14 -0600

    modificaciones

---
+++ asset/js/app.js
@@ -0,0 +1,48 @@
+
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener("click", () => {
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+
+        });
+        burger.classList.toggle('toggle');
+    });
+
+}
+
+navSlide();
+/*********************************Colocar aca el desarrollo de su ejercicio***************************/
+
+var cont e = 1;
+
+var bitacoras = [];
+
+var formulario = document.getElementById("bitacora");
+
+formulario.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    let bitacora = {
+        cant: cont,
+        fecha: formulario[1].value,
+        descripcion: formulario[2].value,
+        cantidad: formulario[3].value
+    }
+    bitacoras.push(bitacora);
+    cont++;
+    mostrar();
+});
+
+
+
